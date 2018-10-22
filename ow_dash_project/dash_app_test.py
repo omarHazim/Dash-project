79b89efdb8c444b0327929c6cdf8750bbb8efc95
omar.hazim <omar.hazim@oliverwyman.com>
2018-10-22 16:53:06 +0200

    add new dash snipts

--- ow_dash_project/dash_app_test.py
+++ ow_dash_project/dash_app_test.py
@@ -13,3 +13,31 @@ import os
 import numpy as np
 from dash.dependencies import Input, Output, State, Event
 #import json
+
+#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash()
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
